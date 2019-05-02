3b116d2c52693ef797e29b1e4bc57daf21172d2d
Olaf Becker <olaf.becker@clio.com>
2019-05-01 19:11:59 -0700

    Initial commit

---
+++ src/server/apiServer.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+
+const ApiServer = {
+    instance: null,
+    initialize: (token) => {
+        console.log(`Initialize axios with bearer token : ${token}`);
+        ApiServer.instance = axios.create({
+            baseURL: 'https://app.clio.com/',
+            headers: { 'Authorization': `Bearer ${token}` }
+        });
+    },
+    get: async (url) => {
+        console.log("ApiServer.get: ", url);
+        if (!ApiServer.instance) {
+            console.error("No Axios instance, need to initialize ApiServer !")
+        }
+        let result = await ApiServer.instance.get(url);
+        return result;
+    }
+};
+
+module.exports = ApiServer;
